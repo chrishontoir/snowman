daaf1378741b52b01abc07026d7d953b8c2bb632
Stephen Rafferty <srafferty73@gmail.com>
2018-12-05 13:55:29 +0000

    get player input done

--- game.rb
+++ game.rb
@@ -1,7 +1,21 @@
 class Game
+  attr_accessor :player, :hidden_word, :guessed_letters
+
   def initialize(player, hidden_word, guessed_letters=[])
     @player = player
     @hidden_word = hidden_word
     @guessed_letters = guessed_letters
   end
+
+  def get_input_from_player
+    @input = gets.chomp
+    return @input
+  end
+
+  def guessed_letters_length
+    return @guessed_letters.length
+  end
+
+
+
 end
--- specs/game_spec.rb
+++ specs/game_spec.rb
@@ -7,7 +7,26 @@ require_relative('../player')
 class GameTest < MiniTest::Test
   def setup
     @player = Player.new("Conor", 6)
-    @hidden_word = HiddenWord.new("CodeClan").downcase
-    @game = Game.new(@player, @hidden_word, guessed_letters[])
+    @hidden_word = HiddenWord.new("codeclan") #downcase to follow...
+    @game = Game.new(@player, @hidden_word,[])
   end
+
+  def test_get_player
+    assert_equal(@player, @game.player)
+  end
+
+  def test_get_hidden_word
+    assert_equal(@hidden_word, @game.hidden_word)
+  end
+
+  def test_get_guessed_letters
+    assert_equal([], @game.guessed_letters)
+  end
+
+  def test_get_input_from_player
+    assert_equal("a", @game.get_input_from_player)
+  end
+
+
+
 end
